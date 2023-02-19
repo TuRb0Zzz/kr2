3a97fc38285920bbd63070efabd12ad7fdf6bf71
Test1Denis <ddd183a_1@mail.ru>
2023-02-19 16:06:07 +0300

    my comments

--- main.c
+++ main.c
@@ -5,47 +5,40 @@
 using namespace std;
 
 
+такой код смотрю последний раз!!!
+
 int main(){
         int step = 100;
-        bool flag1 = 1 , flag2 = 1, flag3 = 1 , flag4 = 1, flag5 = 1 , flag6 = 1;
-        cout << "  bubbleSort  "<<"  selectionSort  "<<"  countsort  "<< "  quickSort  "<<"  insertionSort  "<< "  mergeSort"<<endl;
+        bool flag1 = 1 , flag2 = 1, flag3 = 1 , flag4 = 1, flag5 = 1 , flag6 = 1;	много раз просил так не делать!!!!
+											что это за переменные такие??? можно было объявить массив!!!
+        cout << "  bubbleSort  "<<"  selectionSort  "<<"  countsort  "<< "  quickSort  "<<"  insertionSort  "<< "  mergeSort"<<endl;	пробелы!!!
+            std::chrono::time_point<std::chrono::high_resolution_clock> start;
+            std::chrono::time_point<std::chrono::high_resolution_clock> end;
+            chrono::duration<double> diff;
         for(int arsize = 100; arsize <= 10000000; arsize += step){
             if (arsize == 500){
-
                 step = 250;
-
             }
             if (arsize == 1000){
-
+отличная пустая строчка.... только не понятно зачем она нужна
                 step=1000;
-
+ещё одна.... хмммм...
             }
             if (arsize == 10000){
-
                 step=5000;
-
             }
             if (arsize == 100000){
-
                 step=10000;
-
             }
             if (arsize == 200000){
-
                 step=100000;
-
             }
             if (arsize == 1000000){
-
                 step=1000000;
-
             }
             int* ar = new int[arsize];
             cout<< arsize <<"\t";
             randomFilling(ar , arsize ,0 , 999);
-            std::chrono::time_point<std::chrono::high_resolution_clock> start;
-            std::chrono::time_point<std::chrono::high_resolution_clock> end;
-            chrono::duration<double> diff;
 
             if ( flag1 ){
                     start = chrono::high_resolution_clock::now();
@@ -63,6 +56,18 @@ int main(){
             else {
                     cout << "-----" << "\t";
             }
+
+	здесь надо опять заполнять массив рандомными значениями....
+как думаешь можно ли всё что ты написал превратить в некоторый цикл???
+	1.	выделяем память	
+	2. 	рандомно заполняем массив
+	3.	"запускаем" таймер
+	4.	вызываем нужную сортировку
+	5.	измеряем прошедшее время и выводим на экран (если время сортировки больше 60, исключаем эту сортировку из работы)
+	6.	повторяем с пункта 2
+	7.	освобождаем память задаём новый размер и начинаем с 1ого пункта 
+
+
 
 
             if ( flag2 == 1){
